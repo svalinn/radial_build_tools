0a9d9d929401725330d6684af2a07a26a06e9789
Edgar <ejpflug@gmail.com>
2024-06-07 19:01:38 -0500

    split plotting and model building into two classes, update examples

--- examples/plot_parastell_build_example.py
+++ examples/plot_parastell_build_example.py
@@ -1,83 +1,64 @@
 import numpy as np
-from radial_build import radial_build
+from radial_build_tools import RadialBuildPlot
 
 num_phi = 80
 num_theta = 90
 
-phi_list = np.linspace(0,90,num_phi)
-theta_list = np.linspace(0,360,num_theta)
-ones = np.ones((len(phi_list),len(theta_list)))
+phi_list = np.linspace(0, 90, num_phi)
+theta_list = np.linspace(0, 360, num_theta)
+ones = np.ones((len(phi_list), len(theta_list)))
 
 build = {
-    'phi_list': phi_list,
-    'theta_list': theta_list,
-    'wall_s': 1.2,
-    'radial_build': {
-        'fw': {
-            'thickness_matrix': ones*4, #cell 3
-            'h5m_tag': 'FNSFFW'
+    "phi_list": phi_list,
+    "theta_list": theta_list,
+    "wall_s": 1.2,
+    "radial_build": {
+        "fw": {"thickness_matrix": ones * 4, "h5m_tag": "FNSFFW"},  # cell 3
+        "breeder": {
+            "thickness_matrix": ones * 50,  # cell 4
+            "h5m_tag": "FNSFDCLL",
         },
-        'breeder': {
-            'thickness_matrix': ones*50, #cell 4
-            'h5m_tag':'FNSFDCLL'
+        "BW": {"thickness_matrix": ones * 2, "h5m_tag": "FNSFBW"},  # cell 5
+        "manifolds": {
+            "thickness_matrix": ones * 6,  # cell 6
+            "h5m_tag": "FNSFHeManifolds",
         },
-        'BW': {
-            'thickness_matrix': ones*2, #cell 5
-            'h5m_tag': 'FNSFBW'
+        "HTS": {"thickness_matrix": ones * 20, "h5m_tag": "FNSFIBSR"},  # cell 7
+        "Gap_1": {"thickness_matrix": ones * 1, "h5m_tag": "Vacuum"},  # cell 8
+        "vvfrontplate": {
+            "thickness_matrix": ones * 2,  # cell 9
+            "h5m_tag": "SS316L",
         },
-        'manifolds': {
-            'thickness_matrix': ones*6, #cell 6
-            'h5m_tag': 'FNSFHeManifolds'
+        "VVFill": {
+            "thickness_matrix": ones * 6,  # cell 10
+            "h5m_tag": "VVFill",
         },
-        'HTS': {
-            'thickness_matrix': ones*20, #cell 7
-            'h5m_tag': 'FNSFIBSR'
+        "VVBackPlate": {
+            "thickness_matrix": ones * 2,  # cell 11
+            "h5m_tag": "SS316L",
         },
-        'Gap_1': {
-            'thickness_matrix': ones*1, #cell 8
-            'h5m_tag': 'Vacuum'
+        "Gap_2": {"thickness_matrix": ones * 2, "h5m_tag": "AirSTP"},  # cell 12
+        "LTS": {"thickness_matrix": ones * 23, "h5m_tag": "LTS"},  # cell 13
+        "Thermal_Insulator": {
+            "thickness_matrix": ones * 10,  # cell 14
+            "h5m_tag": "AirSTP",
         },
-        'vvfrontplate': {
-            'thickness_matrix': ones*2, #cell 9
-            'h5m_tag': 'SS316L'
+        "coilfrontplate": {
+            "thickness_matrix": ones * 2,  # cell 15
+            "h5m_tag": "coils",
         },
-        'VVFill': {
-            'thickness_matrix': ones*6, #cell 10
-            'h5m_tag': 'VVFill'
+        "coils": {
+            "thickness_matrix": ones * 50.5,  # cell 16
+            "h5m_tag": "coils",
         },
-        'VVBackPlate': {
-            'thickness_matrix': ones*2, #cell 11
-            'h5m_tag': 'SS316L'
-        },
-        'Gap_2':{
-            'thickness_matrix': ones*2, #cell 12
-            'h5m_tag': 'AirSTP'
-        },
-        'LTS':{
-            'thickness_matrix': ones*23, #cell 13
-            'h5m_tag': 'LTS'
-        },
-        'Thermal_Insulator':{
-            'thickness_matrix': ones*10, #cell 14
-            'h5m_tag': 'AirSTP'
-        },
-        'coilfrontplate':{
-            'thickness_matrix': ones*2, #cell 15
-            'h5m_tag': 'coils'
-        },
-        'coils':{
-            'thickness_matrix': ones*50.5, #cell 16
-            'h5m_tag':'coils'
-        }
-    }
+    },
 }
 
-rb = radial_build.from_parastell_build(
-    build, phi_list[-1], theta_list[-1])
-
+rbp = RadialBuildPlot.from_parastell_build(build, phi_list[-1], theta_list[-1])
 
 # create the radial build plot png
-rb.plot_radial_build(title='Example Parastell Build')
+rbp.title = "Example Parastell Build"
+rbp.to_png()
 
 # save the plot configuration as a yml file
-rb.write_yml()
+rbp.write_yml()
--- examples/plot_radial_build_example.py
+++ examples/plot_radial_build_example.py
@@ -1,58 +1,43 @@
-from radial_build import radial_build
+from radial_build_tools import RadialBuildPlot
 
 build_dict = {
     # here is a layer with no optional data incluede
     "sol": {},
-
     # here is a layer where only thickness is included
     "FW_armor": {"thickness": 0.2},
-
-    "FW": {'thickness': 3.8, 'composition': {"MF82H": 0.34, "HeT410P80": 0.66}},
-
+    "FW": {"thickness": 3.8, "composition": {"MF82H": 0.34, "HeT410P80": 0.66}},
     # here is a layer where only a description is included
-    "breeder": {'description': "Composition and thickness vary"},
-
-    'BW': {'thickness': 2, 'composition': {"MF82H": 0.80, "HeT410P80": 0.20}},
-
+    "breeder": {"description": "Composition and thickness vary"},
+    "BW": {"thickness": 2, "composition": {"MF82H": 0.80, "HeT410P80": 0.20}},
     # here is a layer where thickness and description are included
-    'manifolds': {'thickness': 10, "description": "composition varies"},
-
-    'HTS': {'description': "Composition and thickness vary"},
-
+    "manifolds": {"thickness": 10, "description": "composition varies"},
+    "HTS": {"description": "Composition and thickness vary"},
     # here is a layer where only composition is inclued
-    'gap_1': {'composition': {"Void": 1.0}},
-
-    'vv_front_plate': {'thickness': 2, 'composition': {"SS316L": 1.0}},
-
-    'vv_fill': {'thickness': 6, 'composition': {"SS316L": 0.6, 
-                                                "HeT410P80": 0.4}},
-
-    'vv_back_plate': {'thickness': 2, 'composition': {"SS316L": 1.0}},
-
-    'gap_2': {'thickness': 2, 'composition': {"Void": 1.0}},
-
-    'LTS': {'description': "Composition and thickness vary"},
-
-    'Thermal Insulator (Gap)': {'thickness': 10, 
-                                'composition': {"Void": 1.0}},
-
+    "gap_1": {"composition": {"Void": 1.0}},
+    "vv_front_plate": {"thickness": 2, "composition": {"SS316L": 1.0}},
+    "vv_fill": {
+        "thickness": 6,
+        "composition": {"SS316L": 0.6, "HeT410P80": 0.4},
+    },
+    "vv_back_plate": {"thickness": 2, "composition": {"SS316L": 1.0}},
+    "gap_2": {"thickness": 2, "composition": {"Void": 1.0}},
+    "LTS": {"description": "Composition and thickness vary"},
+    "Thermal Insulator (Gap)": {"thickness": 10, "composition": {"Void": 1.0}},
     # here is a layer with all optional data included
-    'Coil Pack': {'thickness': 52.5, 'composition': {"SS316L": 0.7435, 
-                                                     "HTS TAPE": 0.0622, 
-                                                     "Cu": 0.1307, 
-                                                     "Solder": 0.0438, 
-                                                     "HeT410P80": 0.0288},
-                  'description': '[combines winding pack and coil case]'}
+    "Coil Pack": {
+        "thickness": 52.5,
+        "composition": {
+            "SS316L": 0.7435,
+            "HTS TAPE": 0.0622,
+            "Cu": 0.1307,
+            "Solder": 0.0438,
+            "HeT410P80": 0.0288,
+        },
+        "description": "[combines winding pack and coil case]",
+    },
 }
 
-# note that since this is quite a detailed radial build, the default figure
-# size was insufficient
-rb = radial_build(build_dict)
-
-# create the radial build plot png
-rb.plot_radial_build('Example Radial Build',
-                  max_characters=40, size=(10, 4.2))
-
-# save the plot configuration as a yml file
-rb.write_yml()
-
+rbp = RadialBuildPlot(
+    build_dict, title="Example Radial Build", max_characters=40, size=(8, 4.2)
+)
+rbp.to_png()
