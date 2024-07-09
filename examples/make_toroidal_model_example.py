59fcf432cbc57fd841b2c77b8be9bdf9ebfec807
Edgar <ejpflug@gmail.com>
2024-07-09 10:57:40 -0500

    name radius variables in example script

--- examples/make_toroidal_model_example.py
+++ examples/make_toroidal_model_example.py
@@ -1,10 +1,11 @@
 import openmc
 from radial_build_tools import ToroidalModel, RadialBuildPlot
 
-# wistell d esque values
-a = 800
-b = 300
-c = 100
+# torus parameters
+major_radius = 800
+plasma_minor_z_radius = 300
+plasma_minor_xy_radius = 100
+
 # Define tungsten
 W = openmc.Material(name="Tungsten")
 W.add_element("W", 1.0)
@@ -12,7 +13,7 @@ W.set_density("g/cm3", 19.35)
 
 # Define reduced-activation ferritic martensitic (RAFM) steel
 RAFM = openmc.Material(name="RAFM")
-RAFM.add_element("Fe", 0.895, "wo")  # number, 'wo' --> weight ratio
+RAFM.add_element("Fe", 0.895, "wo")
 RAFM.add_element("Cr", 0.09, "wo")
 RAFM.add_element("W", 0.015, "wo")
 RAFM.set_density("g/cm3", 7.8)
@@ -31,12 +32,14 @@ build = {
     "Breeder": {"thickness": 20, "material": PbLi, "description": PbLi.name},
     "bogus layer": {
         "thickness": 0,
-        "description": "this layer will be skipped",
+        "description": "this layer will be skipped due to zero thickness",
     },
     "shield": {"thickness": 20, "material": W, "description": W.name},
 }
 
-toroidal_model = ToroidalModel(build, 1000, 100, 100)
+toroidal_model = ToroidalModel(
+    build, major_radius, plasma_minor_z_radius, plasma_minor_xy_radius
+)
 model, cells = toroidal_model.get_openmc_model()
 model.export_to_model_xml()
 
