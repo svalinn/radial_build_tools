dade192f0a7ad4f3a2f4a0e2fe8cddd95346a5e6
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-12-06 12:37:39 -0600

    Fix capitalization and add more information to docstrings

--- examples/hcpb_radial_build_example/openmc_Materials_Object_from_json.py
+++ examples/hcpb_radial_build_example/openmc_Materials_Object_from_json.py
@@ -3,10 +3,14 @@ import openmc
 
 "This script makes openmc.Material and openmc.Materials objects using .json files created with fusion-material-db"    
 
-def Make_Material(mixed_mat_data):
+def make_material(mixed_mat_data):
     '''
     Makes openmc.Material objects for each material in .json file (specified when called).
     Adds density & nuclide information to each Material object
+    inputs: 
+        mixed_mat_data : JSON created by HCPB_Mix_Materials
+    outputs:
+        mat_list : list of OpenMC Material objects
     '''
     mat_list = []
     for material, mat_property in mixed_mat_data.items():
@@ -18,9 +22,13 @@ def Make_Material(mixed_mat_data):
             mat_openmc.add_nuclide(element, fraction)       
     return mat_list
 
-def Make_All_Materials(material_list):
+def make_all_materials(material_list):
     '''
     Makes openmc.Materials object using output of Make_Material()
+    inputs:
+        material_list : list of OpenMC Material objects
+    outputs:
+        all_materials : OpenMC Materials object 
     '''
     all_materials = openmc.Materials(material_list)
     all_materials.cross_sections = '../fendl-3.2-hdf5/cross_sections.xml'
