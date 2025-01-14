adc3148462fc83325ba4ac15f6cb534c5790c3e7
anu1217 <asrajendra@wisc.edu>
2025-01-14 16:40:15 -0600

    Change references to material compositions

--- examples/hcpb_radial_build_example/HCPB_Build_Dict.py
+++ examples/hcpb_radial_build_example/HCPB_Build_Dict.py
@@ -3,9 +3,14 @@ import json
 import yaml
 import argparse
 from radial_build_tools import ToroidalModel, RadialBuildPlot
-from HCPB_Mix_Materials import material_comp
+from HCPB_Mix_Materials import make_mat_data
 
-def make_build_dict(scoring_layer_thickness):
+def make_build_dict(mat_info):
+    '''
+    Build a dictionary of material names, thicknesses, compositions, plot colors, and tally scores.
+    input:
+        mat_info : dictionary of material names, compositions, and citations.
+    '''
     build_dict = {
     "sol": {
         "thickness": 5,
@@ -15,156 +20,77 @@ def make_build_dict(scoring_layer_thickness):
     },
     "fw_armor": {
         "thickness": 0.2,
-        "composition" : material_comp['fw_armor'],
-        "material_name": "fw_armor",
         "colour": "#FFD700",  # Gold
         "scores": ["heating"], 
     },
     "fw": {
         "thickness": 3.8,
-        "composition" : material_comp['fw'],
-        "material_name": "fw",
         "colour": "#DAA520",  # Goldenrod
         "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
     },
     "breeder": {
         "thickness": 50,
-        "composition" : material_comp['breeder'],
-        "material_name": "breeder",
         "description": "Thickness Varies",
         "colour": "#B22222",  # Firebrick
         "scores": ["tbr", "heating"],
     },
     "bw": {
         "thickness": 10,
-        "composition": material_comp['bw'],
-        "material_name": "bw",
         "colour": "#8B0000",  # Dark red
         "scores": ["tbr", "heating","fast_fluence"],
     },
-    "manifold_front_plate": {
-        "thickness": 5,
-        "composition": material_comp['manifold_front_plate'],
-        "material_name": "manifold_front_plate",
-        "colour": "#4682B4",  # Steel blue
-        "scores": ["heating"],
-    },
+
     "manifold": {
-        "thickness": 10,
-        "composition": material_comp['manifold'],
-        "material_name": "manifold",
+        "thickness": 20,
         "colour": "#5F9EA0",  # Cadet blue
     },
-    "manifold_back_plate": {
-        "thickness": 5,
-        "composition": material_comp['manifold_back_plate'],
-        "material_name": "manifold_back_plate",
-        "colour": "#4682B4",  # Steel blue
-        "scores": ["heating"],
-    },
-    "hts_front_plate": {
-        "thickness": 1,
-        "composition" : material_comp['hts_front_plate'],
-        "material_name": "hts_front_plate",
-        #"description": "for scoring, same composition as HTS",
-        "colour": "#32CD32",  # Lime green
-        "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
-    },
+    
     "hts": {
-        "thickness": 9,
-        "composition" : material_comp['hts'],
-        "material_name": "hts",
+        "thickness": 10.5,
         "description": "Thickness Varies",
         "colour": "#00FF00",  # Green
         "scores": ["heating"],
     },
-    "hts_back_plate": {
-        "thickness": scoring_layer_thickness,
-        "composition" : material_comp['hts_back_plate'],
-        "material_name": "hts_back_plate",
-        #"description": "for scoring, same composition as HTS",
-        "colour": "#32CD32",  # Lime green
-        "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
-    },
+    
     "gap_1": {
         "thickness": 1,
-        "composition": {"Void": 1.0},
         "colour": "#F5F5F5",  # White smoke
     },
-    "vv_front_plate": {
-        "thickness": 2,
-        "composition" : material_comp['vv_front_plate'],
-        "material_name": "vv_front_plate",
-        "colour": "#A9A9A9",  # Dark grey
-        "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
-    },
+    
     "vv_fill": {
-        "thickness": 6,
-        "composition" : material_comp['vv_fill'],
-        "material_name": "vv_fill",
+        "thickness": 10,
         "colour": "#778899",  # Light slate grey
         "scores": ["heating"],
     },
-    "vv_back_plate": {
-        "thickness": 2,
-        "composition": material_comp['vv_back_plate'],
-        "material_name": "vv_back_plate",
-        "colour": "#A9A9A9",  # Dark grey
-        "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
-    },
+    
     "gap_2": {
         "thickness": 2,
-        "composition": material_comp['gap_2'],
-        "material_name": "gap_2",
         "colour": "#E0FFFF",  # Light cyan
     },
-    "lts_front_plate": {
-        "thickness": scoring_layer_thickness,
-        "composition" : material_comp['lts_front_plate'],
-        "material_name": "lts_front_plate",
-        #"description": "For scoring, same composition as LTS",
-        "colour": "#7FFFD4",  # Aquamarine
-        "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
-    },
+    
     "lts": {
-        "thickness": 9,
-        "composition" : material_comp['lts'],
-        "material_name": "lts",
+        "thickness": 10.5,
         "description": "Thickness Varies",
         "colour": "#00FA9A",  # Medium spring green
         "scores": ["heating"],
     },
-    "lts_back_plate": {
-        "thickness": 1,
-        "composition" : material_comp['lts_back_plate'],
-        "material_name": "lts_back_plate",
-        #"description": "For scoring, same composition as LTS",
-        "colour": "#7FFFD4",  # Aquamarine
-        "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
-    },
+    
     "thermal_insulator": {
         "thickness": 10,
-        "composition": material_comp['thermal_insulator'],
-        "material_name": "thermal_insulator",
         "colour": "#B0E0E6",  # Powder blue
         "scores": ["heating"],
     },
-    "coil_pack_front_plate": {
-        "thickness": 1,
-        "composition" : material_comp['coil_pack_front_plate'],
-        "material_name": "coil_pack_front_plate",
-        #"description": "cell for scoring peak values, same composition as coil_pack",
-        "colour": "#FF6347",  # Tomato
-        "scores": ["fast_fluence", "cu_dpa", "heating"],
-    },
+    
     "coil_pack": {
-        "thickness": 52,
-        "composition" : material_comp['coil_pack'],
-        "material_name": "coil_pack",
+        "thickness": 53,
         "colour": "#FF4500",  # Orange red
         "scores": ["heating"],
     },
 }
+    for component in list(build_dict.keys()):
+        if component in list(mat_info.keys()):
+            build_dict[component]['composition'] = mat_info[component]['composition']
+            build_dict[component]['material_name'] = component
     return build_dict
     
 def make_toroidal_model(materials_xml_file, major_radius, minor_radius_z, minor_radius_xy, build_dict):
@@ -172,7 +98,8 @@ def make_toroidal_model(materials_xml_file, major_radius, minor_radius_z, minor_
         materials = openmc.Materials.from_xml(materials_xml_file)
         toroidal_model = ToroidalModel(
         build_dict, major_radius, minor_radius_z, minor_radius_xy, materials)
-        return toroidal_model
+        mat_geom, cell_dict = toroidal_model.get_openmc_model()
+        return mat_geom, cell_dict
 
 def plot_radial_build(radial_plot_name, build_dict):
         radial_build_plot = RadialBuildPlot(build_dict, title=radial_plot_name, size=(15, 5))
@@ -194,13 +121,16 @@ def main():
    
     args = parse_args()
     yaml_inputs = read_yaml(args)
-    build_dict = make_build_dict(yaml_inputs['geom']['scoring_layer_thickness'])
-    toroidal_model = make_toroidal_model(yaml_inputs['filenames']['mat_xml'],
+    mat_info = make_mat_data()
+    build_dict = make_build_dict(mat_info)
+    mat_geom, cell_dict = make_toroidal_model(yaml_inputs['filenames']['mat_xml'],
                                          yaml_inputs['geom']['major_radius'],
                                          yaml_inputs['geom']['minor_radius_z'],
                                          yaml_inputs['geom']['minor_radius_xy'],
                                          build_dict)
     plot_radial_build(yaml_inputs['filenames']['radial_plot_name'], build_dict)
-
+    #mat_geom, cell_dict = toroidal_model.get_openmc_model()
+    mat_geom.export_to_model_xml(yaml_inputs['filenames']['model_xml_name'])
+    
 if __name__ == "__main__":
     main()    
--- examples/hcpb_radial_build_example/HCPB_Mix_Materials.py
+++ examples/hcpb_radial_build_example/HCPB_Mix_Materials.py
@@ -9,75 +9,77 @@ This script specifies the composition of each HCPB component and its correspondi
 mdbt.mix_by_volume is called to mix individual materials and generate a new .json file.
 '''
 
-def mat_data(): 
+def make_mat_data(): 
     '''    
     Specfies volume fraction and corresponding citation for each radial component.
     '''
-    #Volume fractions of constituent materials
-    material_comp = {
-'fw_armor' : {"W": 1.00},
-'fw' : {"W": 0.1, "EUROFER97" : 0.9},
-'breeder' : {"Be12Ti": 0.8, "Li4SiO4Li60.0":0.14837, "Li2TiO3Li60.0":0.05163}, #with mol% to vol% conversion
-#'breeder' : {"Li4SiO4Li60.0":0.65, "Li2TiO3Li60.0":0.35}, #need to convert mol% to vol%
-'bw' : {"MF82H" : 0.8, "HeNIST" : 0.2},
-'manifold_front_plate' : {"EUROFER97": 1.0},
-'manifold' : {"HeT410P80": 1.00},
-'manifold_back_plate' : {"HeT410P80": 1.00},
-'hts_front_plate' : {"MF82H": 0.20, "HeNIST": 0.28, "BMF82H" : 0.52},
-'hts': {"MF82H": 0.20, "HeNIST": 0.28, "BMF82H" : 0.52},
-'hts_back_plate' : {"MF82H": 0.20, "HeNIST": 0.28, "BMF82H" : 0.52},
-'vv_front_plate' : {"HeNIST" : 0.4, "Cr3FS" : 0.6}, #changed from SS316L in corresponding build_dict
-'vv_fill' : {"HeNIST" : 0.4, "Cr3FS" : 0.6},
-'vv_back_plate' : {"HeNIST" : 0.4, "Cr3FS" : 0.6}, #changed from SS316L in corresponding build_dict
-'gap_2' : {"AirSTP": 1.0},
-'lts_front_plate' : {"Cr3FS": 0.39, "BMF82H": 0.29, "Water": 0.32},
-'lts' : {"Cr3FS": 0.39, "BMF82H": 0.29, "Water": 0.32},
-'lts_back_plate' : {"Cr3FS": 0.39, "BMF82H": 0.29, "Water": 0.32},
-'thermal_insulator' : {'AirSTP': 1.0},
-'coil_pack_front_plate' : {"JK2LBSteel": 0.3, "Cu": 0.25, "TernaryNb3Sn" : 0.25, "Eins" : 0.1, "HeNIST" : 0.1},
-'coil_pack' : {"JK2LBSteel": 0.3, "Cu": 0.25, "TernaryNb3Sn" : 0.25, "Eins" : 0.1, "HeNIST" : 0.1},
-}
+    #Volume fractions of constituent materials and corresponding citations
+    mat_info = {
+    'fw_armor' : {
+        'composition' : {"W": 1.00},
+        'citation' : "ZhouEUDEMOHCPB_2023"
+        },
+    'fw' : {
+        'composition' : {"W": 0.1, "EUROFER97" : 0.9},
+        'citation' : "ZhouEUDEMOHCPB_2023"
+        },
+    'breeder' : {
+        'composition' : {"Be12Ti": 0.8, "Li4SiO4Li60.0":0.14837, "Li2TiO3Li60.0":0.05163},
+        'citation' : "ZhouEUDEMOHCPB_2023"
+        },
+    'bw' : {
+        'composition' : {"MF82H" : 0.8, "HeNIST" : 0.2},
+        'citation' : "DavisFusEngDes_2018"  
+        },
+    'manifold' : {
+        'composition' : {"HeT410P80": 1.00},
+        'citation' : "DavisFusEngDes_2018"
+        },
+    'hts' : {
+        'composition' : {"MF82H": 0.20, "HeNIST": 0.28, "BMF82H" : 0.52},
+        'citation' : "DavisFusEngDes_2018"
+        },
+    'vv_fill' : {
+        'composition' : {"HeNIST" : 0.4, "Cr3FS" : 0.6},
+        'citation' : "DavisFusEngDes_2018"
+        },
+    'gap_2' : {
+        'composition' : {"AirSTP": 1.0},
+        'citation' : "ZhouEUDEMOHCPB_2023"
+        },
+    'lts' : {
+        'composition' : {"Cr3FS": 0.39, "BMF82H": 0.29, "Water": 0.32},
+        'citation' : "DavisFusEngDes_2018"
+        },
+    'thermal_insulator':{
+        'composition' : {'AirSTP': 1.0},
+        'citation' : "ZhouEUDEMOHCPB_2023"
+        },
+    'coil_pack': {
+        'composition' : {"JK2LBSteel": 0.3, "Cu": 0.25, "TernaryNb3Sn" : 0.25, "Eins" : 0.1, "HeNIST" : 0.1},
+        'citation' : "DavisFusEngDes_2018"
+        },       
+    }    
+    return mat_info
 
-    citation_list = {
-    'fw_armor' : "ZhouEUDEMOHCPB_2023",
-    'fw' : "ZhouEUDEMOHCPB_2023",
-    'breeder' : "ZhouEUDEMOHCPB_2023",
-    'bw' : "DavisFusEngDes_2018",
-    'manifold_front_plate' : "DavisFusEngDes_2018",
-    'manifold' : "DavisFusEngDes_2018",
-    'manifold_back_plate' : "DavisFusEngDes_2018",
-    'hts_front_plate' : "DavisFusEngDes_2018",
-    'hts' : "DavisFusEngDes_2018",
-    'hts_back_plate' : "DavisFusEngDes_2018",
-    'vv_front_plate' : "DavisFusEngDes_2018",
-    'vv_fill' : "DavisFusEngDes_2018",
-    'vv_back_plate' : "DavisFusEngDes_2018",
-    'gap_2' : "ZhouEUDEMOHCPB_2023" ,
-    'lts_front_plate' : "DavisFusEngDes_2018",
-    'lts' : "DavisFusEngDes_2018",
-    'lts_back_plate' : "DavisFusEngDes_2018",
-    'thermal_insulator' : "ZhouEUDEMOHCPB_2023",
-    'coil_pack_front_plate' : "DavisFusEngDes_2018",
-    'coil_pack' : "DavisFusEngDes_2018",
-    }
-    return material_comp, citation_list
-
-def make_mixed_mat_lib(material_comp, citation_list, pure_mat_json):
+def make_mixed_mat_lib(mat_info, pure_mat_json):
     '''
     Creates dictionary consisting of material name, composition, and citation.
+    
+    pure_mat_json : path to .json containing pure material compositions
     '''
-    mat_data = {}
-    for mat_name, comp in material_comp.items():
+    mat_data = {} 
+    for mat_name, comp_cit in mat_info.items():
         mat_data[mat_name] = {
-              "vol_fracs": comp,
-              "mixture_citation" : citation_list[mat_name],
-          } 
+            'vol_fracs' : comp_cit['composition'],
+            'mixture_citation' : comp_cit['citation'],
+            }
 
     # Load material library
     mat_lib = mdbt.MaterialLibrary()
     mat_lib.from_json(pure_mat_json)
 
-    # create material library object
+    # Create PyNE material library object
     mixmat_lib = mdbt.MaterialLibrary()
     for mat_name, mat_input in mat_data.items():
         mixmat_lib[mat_name] = mdbt.mix_by_volume(
@@ -99,8 +101,8 @@ def main():
     
     args = parse_args()
     yaml_inputs = read_yaml(args)
-    material_comp, citation_list = mat_data()
-    mixmat_lib = make_mixed_mat_lib(material_comp, citation_list,
+    mat_info = make_mat_data()
+    mixmat_lib = make_mixed_mat_lib(mat_info,
                                     yaml_inputs['filenames']['pure_mat_json'])
     # write OpenMC Materials xml 
     mixmat_lib.write_openmc(yaml_inputs['filenames']['mat_xml'])
