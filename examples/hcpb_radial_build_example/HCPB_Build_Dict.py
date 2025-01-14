42ba0da1e437f34e35d49be1a649944dca6b9d53
anu1217 <asrajendra@wisc.edu>
2025-01-14 10:20:00 -0600

    Change composition to match HCPB_Mix_Materials.py dictionary and load Materials object from xml

--- examples/hcpb_radial_build_example/HCPB_Build_Dict.py
+++ examples/hcpb_radial_build_example/HCPB_Build_Dict.py
@@ -1,18 +1,15 @@
 import openmc
+import json
 from radial_build_tools import ToroidalModel, RadialBuildPlot
-from openmc_Materials_Object_from_json import *
-
-with open('mixedPureFusionMatsHCPB_libv1.json', 'r') as mixed_mat_json:
-    mixed_mat = json.load(mixed_mat_json)
+from HCPB_Mix_Materials import material_comp
 
 major_radius = 800
 minor_radius_z = 114
 minor_radius_xy = 114
 scoring_layer_thickness = 0.5
 
-make_mat = Make_Material(mixed_mat)
 # openmc Materials object:
-materials = Make_All_Materials(make_mat)
+materials = openmc.Materials.from_xml("mixedPureFusionMatsHCPB_libv1.xml")
 
 build_dict = {
     "sol": {
@@ -23,21 +20,21 @@ build_dict = {
     },
     "fw_armor": {
         "thickness": 0.2,
-        #"composition": mixed_materials["FWWArmor"]["vol_fracs"],
+        "composition" : material_comp['fw_armor'],
         "material_name": "fw_armor",
         "colour": "#FFD700",  # Gold
         "scores": ["heating"], 
     },
     "fw": {
         "thickness": 3.8,
-        #"composition": mixed_materials["FW"]["vol_fracs"],
+        "composition" : material_comp['fw'],
         "material_name": "fw",
         "colour": "#DAA520",  # Goldenrod
         "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
     },
     "breeder": {
         "thickness": 50,
-        "composition": {"BreederHCPB": 1.0},
+        "composition" : material_comp['breeder'],
         "material_name": "breeder",
         "description": "Thickness Varies",
         "colour": "#B22222",  # Firebrick
@@ -45,33 +42,34 @@ build_dict = {
     },
     "bw": {
         "thickness": 10,
-        "composition": {"BWHCPB": 1.0},
+        "composition": material_comp['bw'],
         "material_name": "bw",
         "colour": "#8B0000",  # Dark red
         "scores": ["tbr", "heating","fast_fluence"],
     },
     "manifold_front_plate": {
         "thickness": 5,
-        "composition": {"EUROFER97": 1.0},
+        "composition": material_comp['manifold_front_plate'],
         "material_name": "manifold_front_plate",
         "colour": "#4682B4",  # Steel blue
         "scores": ["heating"],
     },
     "manifold": {
         "thickness": 10,
-        "composition": {"HeT410P80": 1.0},
+        "composition": material_comp['manifold'],
         "material_name": "manifold",
         "colour": "#5F9EA0",  # Cadet blue
     },
     "manifold_back_plate": {
         "thickness": 5,
-        "composition": {"EUROFER97": 1.0},
+        "composition": material_comp['manifold_back_plate'],
         "material_name": "manifold_back_plate",
         "colour": "#4682B4",  # Steel blue
         "scores": ["heating"],
     },
     "hts_front_plate": {
-        "thickness": scoring_layer_thickness,
+        "thickness": 1,
+        "composition" : material_comp['hts_front_plate'],
         "material_name": "hts_front_plate",
         #"description": "for scoring, same composition as HTS",
         "colour": "#32CD32",  # Lime green
@@ -79,7 +77,7 @@ build_dict = {
     },
     "hts": {
         "thickness": 9,
-        #"composition":  mixed_materials["FNSFIBSR"]["vol_fracs"],
+        "composition" : material_comp['hts'],
         "material_name": "hts",
         "description": "Thickness Varies",
         "colour": "#00FF00",  # Green
@@ -87,6 +85,7 @@ build_dict = {
     },
     "hts_back_plate": {
         "thickness": scoring_layer_thickness,
+        "composition" : material_comp['hts_back_plate'],
         "material_name": "hts_back_plate",
         #"description": "for scoring, same composition as HTS",
         "colour": "#32CD32",  # Lime green
@@ -99,34 +98,34 @@ build_dict = {
     },
     "vv_front_plate": {
         "thickness": 2,
-        #"composition": mixed_materials["VVFill"]["vol_fracs"],
+        "composition" : material_comp['vv_front_plate'],
         "material_name": "vv_front_plate",
         "colour": "#A9A9A9",  # Dark grey
         "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
     },
     "vv_fill": {
         "thickness": 6,
-        "composition": {"VVFill": 1.0},
+        "composition" : material_comp['vv_fill'],
         "material_name": "vv_fill",
         "colour": "#778899",  # Light slate grey
         "scores": ["heating"],
     },
     "vv_back_plate": {
         "thickness": 2,
-        "composition": {"SS316L": 1.0},
+        "composition": material_comp['vv_back_plate'],
         "material_name": "vv_back_plate",
         "colour": "#A9A9A9",  # Dark grey
         "scores": ["fast_fluence", "he_prod", "fe_dpa", "heating"],
     },
     "gap_2": {
         "thickness": 2,
-        "composition": {"AirSTP": 1.0},
+        "composition": material_comp['gap_2'],
         "material_name": "gap_2",
         "colour": "#E0FFFF",  # Light cyan
     },
     "lts_front_plate": {
         "thickness": scoring_layer_thickness,
-        #"composition": {"LTS": 1.0},
+        "composition" : material_comp['lts_front_plate'],
         "material_name": "lts_front_plate",
         #"description": "For scoring, same composition as LTS",
         "colour": "#7FFFD4",  # Aquamarine
@@ -134,15 +133,15 @@ build_dict = {
     },
     "lts": {
         "thickness": 9,
-        "composition": {"LTS": 1.0},
+        "composition" : material_comp['lts'],
         "material_name": "lts",
         "description": "Thickness Varies",
         "colour": "#00FA9A",  # Medium spring green
         "scores": ["heating"],
     },
     "lts_back_plate": {
-        "thickness": scoring_layer_thickness,
-        #"composition": {"LTS": 1.0},
+        "thickness": 1,
+        "composition" : material_comp['lts_back_plate'],
         "material_name": "lts_back_plate",
         #"description": "For scoring, same composition as LTS",
         "colour": "#7FFFD4",  # Aquamarine
@@ -150,13 +149,14 @@ build_dict = {
     },
     "thermal_insulator": {
         "thickness": 10,
-        "composition": {"AirSTP": 1.0},
+        "composition": material_comp['thermal_insulator'],
         "material_name": "thermal_insulator",
         "colour": "#B0E0E6",  # Powder blue
         "scores": ["heating"],
     },
     "coil_pack_front_plate": {
-        "thickness": scoring_layer_thickness,
+        "thickness": 1,
+        "composition" : material_comp['coil_pack_front_plate'],
         "material_name": "coil_pack_front_plate",
         #"description": "cell for scoring peak values, same composition as coil_pack",
         "colour": "#FF6347",  # Tomato
@@ -164,7 +164,7 @@ build_dict = {
     },
     "coil_pack": {
         "thickness": 52,
-        "composition": {"coils": 1.0},
+        "composition" : material_comp['coil_pack'],
         "material_name": "coil_pack",
         "colour": "#FF4500",  # Orange red
         "scores": ["heating"],
@@ -174,3 +174,7 @@ build_dict = {
 toroidal_model = ToroidalModel(
     build_dict, major_radius, minor_radius_z, minor_radius_xy, materials
 )
+
+rbp = RadialBuildPlot(build_dict, title="HCPB Toroidal Model", size=(15, 5))
+rbp.plot_radial_build()
+rbp.to_png()
